fe62594970a9ef9c8a74c5bdb381328f62787f08
Skyler Jones <skylerjones@Skylers-MacBook-Air.local>
2024-06-05 21:40:06 -0500

    first draft of models

---
+++ Models/Requests.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('');
+
+class Requests extends Model {};
+
+Requests.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING(500),
+            allowNull: false,
+        },
+        powerLevel: {
+            type: DataTypes.STRING(1),
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'request',
+    }
+);
+
+module.exports = Requests;
