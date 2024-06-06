985c4022167c563ca4dc8c5b2f3c1462fed84c89
akosla00 <akosla00@gmail.com>
2024-06-05 22:24:02 -0400

    created package.json and installed npm packages

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const htmlContoller = require('./htmlController');
+const userController = require('./userController');
+const heroController = reuqire('./heroController');
+
+module.exports = { htmlContoller, userController, heroController };
