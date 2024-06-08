18adbabb21c1e07309c220cfb37982e184453059
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-06-08 18:03:55 -0400

    added the sign up page with updated styling, no js work on sign up yet

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,12 +1,11 @@
 module.exports = {
-	homeroute: (req, res) => {
+	home: (req, res) => {
 		res.render('home');
-	},
-	addComment: (req, res) => {
-		//do work as a post create a new comment
-		res.render('commentAdded');
 	},
     login: (req, res) => {
         res.render('login');
-    }
+    },
+    signUp: (req, res) => {
+        res.render('signUp');
+    },
 };
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const htmlContoller = require('../../controllers/htmlController');
 
 router.route('/login').get(htmlContoller.login);
-// router.route('/').get(htmlContoller.homepage);
+router.route('/sign-up').get(htmlContoller.signUp);
+// router.route('/').get(htmlContoller.home);
 
 module.exports = router;
