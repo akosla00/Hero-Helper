5e758eb979c8805d210cec75862c41028fa0d957
lyoko90210 <danieldennis801@gmail.com>
2024-06-10 21:54:34 -0400

    maps page

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -2,8 +2,20 @@ module.exports = {
 	home: (req, res) => {
 		res.render('home');
 	},
+<<<<<<< Updated upstream
 	login: (req, res) => {
 		//do work as a post create a new comment
 		res.render('login');
 	}
+=======
+    login: (req, res) => {
+        res.render('login');
+    },
+    signUp: (req, res) => {
+        res.render('signUp');
+    },
+	map: (req, res) => {
+		res.render('map');
+	},
+>>>>>>> Stashed changes
 };
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const htmlContoller = require('../../controllers/htmlController');//This connects to the controller folder to the htmlController.js file
 
+<<<<<<< Updated upstream
 // router.route('/login').get(htmlContoller.signup);
+=======
+router.route('/login').get(htmlContoller.login);
+router.route('/sign-up').get(htmlContoller.signUp);
+router.route('/map').get(htmlContoller.map);
+>>>>>>> Stashed changes
 router.route('/').get(htmlContoller.home);//landing page that will display your homepage
 
 module.exports = router;
