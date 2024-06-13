1aad1fbef089766c2425b830f9aac3afd73df9bf
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-06-12 22:26:43 -0400

    combining

---
+++ controllers/requestController.js
@@ -0,0 +1,10 @@
+// const { Requests } = require('../Models');
+
+// module.exports = {
+//     add: async (req, res) => {
+//         const requestData = await Requests.create(req.body);
+//         req.session.save(() => {
+
+//         })
+//     }
+// }
