9bf23bd9c31bdfc187f1c972748f0ad6404081ad
akosla00 <akosla00@gmail.com>
2024-06-13 20:23:38 -0400

    added withauth, edited buttons

--- routes/apiRoutes/requestRoutes.js
+++ routes/apiRoutes/requestRoutes.js
@@ -1,9 +1,8 @@
 const requestController = require('../../controllers/requestController');
-const { withAuth } = require('../../utils/Auth');
 
 const router = require('express').Router();
 
-router.route('/').post(withAuth, requestController.add).get(requestController.all);
+router.route('/').post(requestController.add).get(requestController.all);
 
 
 module.exports = router
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const htmlContoller = require('../../controllers/htmlController');//This connects to the controller folder to the htmlController.js file
+const { withAuth } = require('../../utils/Auth');
 
 router.route('/login').get(htmlContoller.login);
 router.route('/logout').get(htmlContoller.logout);
 router.route('/sign-up').get(htmlContoller.signUp);
-router.route('/requests').get(htmlContoller.requests);
+router.route('/requests').get(withAuth, htmlContoller.requests);
 router.route('/map').get(htmlContoller.map);
 router.route('/profile').get(htmlContoller.profile);
 router.route('/').get(htmlContoller.home);//landing page that will display your homepage
