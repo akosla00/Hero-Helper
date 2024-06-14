cc3b42707e51cd3fa322d6f3e3658abe5e637f15
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-06-13 20:29:47 -0400

    not done with profile

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const htmlContoller = require('../../controllers/htmlController');//This connects to the controller folder to the htmlController.js file
+const { withAuth } = require('../../utils/Auth');
 
 router.route('/login').get(htmlContoller.login);
 router.route('/logout').get(htmlContoller.logout);
 router.route('/sign-up').get(htmlContoller.signUp);
 router.route('/requests').get(htmlContoller.requests);
 router.route('/map').get(htmlContoller.map);
-router.route('/profile').get(htmlContoller.profile);
+router.route('/profile').get(withAuth, htmlContoller.profile);
 router.route('/').get(htmlContoller.home);//landing page that will display your homepage
 
 module.exports = router;
