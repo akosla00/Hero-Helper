b63fb106d962b04ec712003cc1ba3c8c34928b13
akosla00 <akosla00@gmail.com>
2024-06-13 20:29:30 -0400

    fixed login bug

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,29 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#inputEmail').value.trim();
-    const password = document.querySelector('#inputPasword').value.trim();
-    const emailPassword = {
-      email: email,
-      password: password
-    };
+  event.preventDefault();
 
-    if (email && password) {
-
-      const response = await fetch('/api/user/login', {
-        method: 'POST',
-        body: JSON.stringify(emailPassword),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
-    }
+  const email = document.querySelector('#inputEmail').value.trim();
+  const password = document.querySelector('#inputPasword').value.trim();
+  const emailPassword = {
+    email: email,
+    password: password
   };
-  
-  document
-    .querySelector('#login')
-    .addEven  tListener('submit', loginFormHandler);
+
+  if (email && password) {
+
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify(emailPassword),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
+    }
+  }
+};
+
+document
+  .querySelector('#login')
+  .addEventListener('submit', loginFormHandler);
