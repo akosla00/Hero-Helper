c489e058a08dee0772ccc7eee50ee1674bc105d9
akosla00 <akosla00@gmail.com>
2024-06-13 22:06:32 -0400

    basic about page created

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const htmlContoller = require('../../controllers/htmlController');//This connects to the controller folder to the htmlController.js file
+const htmlController = require('../../controllers/htmlController');//This connects to the controller folder to the htmlController.js file
 const { withAuth } = require('../../utils/Auth');
 
-router.route('/login').get(htmlContoller.login);
-router.route('/logout').get(htmlContoller.logout);
-router.route('/sign-up').get(htmlContoller.signUp);
-router.route('/requests').get(htmlContoller.requests);
-router.route('/heroes').get(htmlContoller.heroes);
-router.route('/map').get(htmlContoller.map);
-router.route('/profile').get(withAuth, htmlContoller.profile);
-router.route('/').get(htmlContoller.home);//landing page that will display your homepage
+router.route('/login').get(htmlController.login);
+router.route('/logout').get(htmlController.logout);
+router.route('/sign-up').get(htmlController.signUp);
+router.route('/requests').get(htmlController.requests);
+router.route('/heroes').get(htmlController.heroes);
+router.route('/map').get(htmlController.map);
+router.route('/profile').get(withAuth, htmlController.profile);
+router.route('/about').get(htmlController.about);
+router.route('/').get(htmlController.home);//landing page that will display your homepage
 
 module.exports = router;
